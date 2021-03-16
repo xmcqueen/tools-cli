8f95fdd2e834e61e1aba3ce711dc16c4b754ef3b
root <bmcqueen@linkedin.com>
2021-03-16 01:30:32 +0000

    tidy up

--- src/command_line.h
+++ src/command_line.h
@@ -32,22 +32,4 @@ private:
     std::unique_ptr<Template> m_tpl;
 };
 
-/**
- *  * CommandLine is analog CLI of modern python j2cli (https://github.com/kolypto/j2cli)
- *   */
-class CommandLineJ2 : protected ICommands {
-public:
-    explicit CommandLineJ2(std::unique_ptr<Template> tpl = std::make_unique<Template>());
-
-    int Execute(int argc, char **argv);
-
-protected:
-    std::string Help() override ;
-    std::string Version() override;
-    void RenderTemplate(const std::string& fileName) override;
-
-private:
-    std::unique_ptr<Template> m_tpl;
-};
-
 } // namespace jinja2
